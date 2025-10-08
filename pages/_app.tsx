898b2cad89492ee49082ad8fd33c77f69de015ec
Adithya Dasarathi <adasarathi@hotmail.com>
2025-10-07 21:00:01 -0400

    Fix: Add inline styles for full screen display without build timeout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,32 @@
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <style jsx global>{`
+        html, body {
+          margin: 0;
+          padding: 0;
+          height: 100%;
+          width: 100%;
+          box-sizing: border-box;
+        }
+        *, *::before, *::after {
+          box-sizing: inherit;
+        }
+        #__next {
+          height: 100%;
+          width: 100%;
+        }
+        body {
+          font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+            'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+            sans-serif;
+          -webkit-font-smoothing: antialiased;
+          -moz-osx-font-smoothing: grayscale;
+        }
+      `}</style>
+      <Component {...pageProps} />
+    </>
+  )
 }
